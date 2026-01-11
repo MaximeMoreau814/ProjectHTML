a01979a2ae749c102eec18c32caabc6b3465736f
Remi118218 <remi.reulier@orange.fr>
2026-01-11 01:45:11 +0100

    better roomgame

--- Page web/scriptCréer.js
+++ Page web/scriptCréer.js
@@ -9,64 +9,77 @@ window.addEventListener('unload', function (e) {
     localStorage.clear();
 });
 
-if(!localStorage.getItem('roomcode')){
-    let code="";
-    function generateCode() {
-        const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const backBtn = document.getElementById("back-btn");
+    backBtn.addEventListener("click", function() {
+    window.location.href = "index.html";
+});
 
-        for (let i = 0; i < 5; i++) {
-            code += letters[Math.floor(Math.random()*26)];
-        }
-        //document.getElementById("code-box").textContent = code;
-        fetch('http://localhost:8080/create?r='+code+"&user="+localStorage.getItem('username')).then(response => response.text()).then(data => {
-            if(data =! "true"){
-                generateCode();
+const copyBtn = document.getElementById("copy-btn");
+    copyBtn.addEventListener("click", function() {
+    navigator.clipboard.writeText(text).then(() => {
+        alert("Code copié : " + text);
+    }).catch(() => {
+        alert("Impossible de copier le code !");
+    });
+});
+
+const startBtn = document.getElementById("start-btn");
+    startBtn.addEventListener("click", function() {
+    localStorage.setItem("start","true");
+    main();
+});
+
+function main(){
+    if(!localStorage.getItem('start')){
+        if(!localStorage.getItem('roomcode')){
+            let code="";
+            function generateCode() {
+                const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+                for (let i = 0; i < 5; i++) {
+                    code += letters[Math.floor(Math.random()*26)];
+                }
+                //document.getElementById("code-box").textContent = code;
+                fetch('http://localhost:8080/create?r='+code+"&user="+localStorage.getItem('username')).then(response => response.text()).then(data => {
+                    if(data =! "true"){
+                        generateCode();
+                    }
+                });
             }
-        });
+            generateCode();
+            localStorage.setItem('roomcode',code);
+            let text=code;
+            document.getElementById("code-box").textContent = text;
+
+        }
+        else{
+            let text=localStorage.getItem('roomcode');
+            document.getElementById("code-box").textContent = text;
+            console.log(text);
+        }
     }
-    generateCode();
-    localStorage.setItem('roomcode',code);
-    let text=code;
-    document.getElementById("code-box").textContent = text;
-
-    const backBtn = document.getElementById("back-btn");
-            backBtn.addEventListener("click", function() {
-                window.location.href = "index.html";
-            });
-
-    const copyBtn = document.getElementById("copy-btn");
-    copyBtn.addEventListener("click", function() {
-        navigator.clipboard.writeText(text).then(() => {
-            alert("Code copié : " + text);
-        }).catch(() => {
-            alert("Impossible de copier le code !");
-        });
-    });
-    const startBtn = document.getElementById("start-btn");
-        startBtn.addEventListener("click", function() {
-        window.location.href = "Game.html";
-    });
+    else{
+        console.log("ture");
+        var element = document.getElementById("code-box");
+        element.remove();
+        element=document.createElement("p");
+        element.className="question";
+        element.textContent="Quoi ?";
+        document.body.replaceChild(element,document.getElementById("indication"));
+        element=document.createElement("div");
+        element.className("reponses")
+        child=document.createElement("button");
+        child.setAttribute("id","button1");
+        child.className("btn btn-rep");
+        child.textContent="Premiere personne"
+        element.appendChild(child);
+        document.body.replaceChild(element,document.getElementById("start-btn"));
+    }
+    document.getElementById("code").textContent = localStorage.getItem('roomcode');
 }
-else{
-    let text=localStorage.getItem('roomcode');
-    document.getElementById("code-box").textContent = text;
-    console.log(text);
 
-    const backBtn = document.getElementById("back-btn");
-            backBtn.addEventListener("click", function() {
-                window.location.href = "index.html";
-            });
-
-    const copyBtn = document.getElementById("copy-btn");
-    copyBtn.addEventListener("click", function() {
-        navigator.clipboard.writeText(text).then(() => {
-            alert("Code copié : " + text);
-        }).catch(() => {
-            alert("Impossible de copier le code !");
-        });
-    });
-    const startBtn = document.getElementById("start-btn");
-        startBtn.addEventListener("click", function() {
-        window.location.href = "Game.html";
-    });
-}
+main();
+/*
+        <div class="reponses">
+            <button id="button1" class="btn btn-rep">Premiere personne</button>
+        </div>*/
