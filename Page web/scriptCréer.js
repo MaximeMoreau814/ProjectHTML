981939866bf8cd3b9aa722c2675c6c79914851c1
Clemengineer <clgautret@gmail.com>
2026-01-12 16:00:42 +0000

    Rectification liens de navigation

--- Page web/scriptCréer.js
+++ Page web/scriptCréer.js
@@ -11,7 +11,7 @@ window.addEventListener('unload', function (e) {
 
 const backBtn = document.getElementById("back-btn");
     backBtn.addEventListener("click", function() {
-    window.location.href = "index.html";
+    window.location.href = "Index.html";
 });
 
 const copyBtn = document.getElementById("copy-btn");
